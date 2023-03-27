9e2513e0402c555c7dc80e2d5f6232e802378562
akhmedmukhtarov <109713492+akhmedmukhtarov@users.noreply.github.com>
2023-03-27 15:20:42 +0500

    product search

--- src/modules/product/dto/findAll-product.dto.ts
+++ src/modules/product/dto/findAll-product.dto.ts
@@ -1,6 +1,7 @@
 export class FindAllProductDto {
-    page: string
-    limit: string
-    attr: string
+    page?: string
+    limit?: string
+    attr?: string
+    search?: string
 
   }
--- src/modules/product/services/findAll-product.service.ts
+++ src/modules/product/services/findAll-product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Like } from 'typeorm';
 import { FindAllProductDto } from '../dto/findAll-product.dto';
 import { Product } from '../entities/product.entity';
 
@@ -6,20 +7,62 @@ import { Product } from '../entities/product.entity';
 export class FindAllProductService {
     async findAll(findAllProductDto: FindAllProductDto) {
         try {
-            let {page, limit, attr}:any = findAllProductDto
-            page = +page
-            limit = +limit
-            limit = limit || 15
-            limit = limit > 15  ? 15  :limit
-            const skippedItems = (+page || 1 - 1) * (+limit);
-            const products = await Product.find({
-                take: limit,
-                skip: skippedItems
-            })
+            let { page, limit, attr, search }: any = findAllProductDto;
+            page = +page;
+            limit = +limit;
+            limit = limit || 15;
+            limit = limit > 15 ? 15 : limit;
+            const skippedItems = (page || 1 - 1) * +limit;
 
-            return products
+            if (attr && search) {
+                const attributeValues = [];
+                attr = attr.split(',').map((el) => +el);
+                for (const attrValueId of attr) {
+                    attributeValues.push({ id: attrValueId });
+                }
 
-            
+                const products = await Product.find({
+                    where: {
+                        attributeValues,
+                        nameUz: Like(`%${search}%`)
+                    },
+                    take: limit,
+                    skip: skippedItems,
+                });
+                return products
+
+            } else if (attr) {
+                const attributeValues = [];
+                attr = attr.split(',').map((el) => +el);
+                for (const attrValueId of attr) {
+                    attributeValues.push({ id: attrValueId });
+                }
+
+                const products = await Product.find({
+                    where: {
+                        attributeValues,
+                    },
+                    take: limit,
+                    skip: skippedItems,
+                });
+
+                return products;
+            } else if (search) {
+                const products = await Product.find({
+                    take: limit,
+                    skip: skippedItems,
+                    where: {
+                        nameUz: Like(`%${search}%`)
+                    }
+                })
+                return products
+            } else {
+                const products = await Product.find({
+                    take: limit,
+                    skip: skippedItems,
+                });
+                return products
+            }
         } catch (err) {
             throw err;
         }
