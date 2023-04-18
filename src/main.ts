c5f159e761073d92badcb497caa2a770323c834b
akhmedmukhtarov <109713492+akhmedmukhtarov@users.noreply.github.com>
2023-04-18 19:13:24 +0500

    extract port to env

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './common/configs/swagger.config';
+import { log } from 'console';
 
 async function bootstrap() {
+  const port = +process.env.PORT
   const app = await NestFactory.create(AppModule);
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}))
-  await app.listen(3000);
+  await app.listen(port).then(()=> console.log(port));
 }
 bootstrap();
